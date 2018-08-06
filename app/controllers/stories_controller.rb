85a347381b25e193cfd281bd2826702481490c53
Jonatan Bonilla <pescadin138@gmail.com>
2018-08-05 23:19:42 -0500

    create story controller logic and routes

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -1,4 +1,21 @@
 class StoriesController < ApplicationController
   def index
+    # @story = Story.find_by(name: 'SitePoint Forums')
+    @story = Story.order('RANDOM()').first
+  end
+
+  def new
+    @story = Story.new
+  end
+
+  def create
+    @story = Story.new(story_params)
+    @story.save
+    redirect_to stories_path
+  end
+
+  private
+  def story_params
+    params.require(:story).permit(:name, :link)
   end
 end
