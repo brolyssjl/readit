8405a53348e38547707f01a77c045ec0ca46e97e
Jonatan Bonilla <pescadin138@gmail.com>
2018-08-12 22:43:40 -0500

    add show view and action to story controller. Overwirte to_param function in story model for friendly-urls

--- app/models/story.rb
+++ app/models/story.rb
@@ -2,4 +2,8 @@ class Story < ApplicationRecord
   # attribute :is_publish, :boolean
   validates :name, :link, presence: true
   has_many :votes
+
+  def to_param
+    "#{id}-#{name.gsub(/\W/, '-').downcase}"
+  end
 end
