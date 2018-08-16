dc13a1ab2edd388d9285d28a4749f56f49bd8a73
Jonatan Bonilla <pescadin138@gmail.com>
2018-08-15 22:53:51 -0500

    Add partial for votes history. Add css and js for that functionality

--- app/models/story.rb
+++ app/models/story.rb
@@ -1,7 +1,11 @@
 class Story < ApplicationRecord
   # attribute :is_publish, :boolean
   validates :name, :link, presence: true
-  has_many :votes
+  has_many :votes do
+    def latest
+      order('id DESC').limit(3)
+    end
+  end
 
   def to_param
     "#{id}-#{name.gsub(/\W/, '-').downcase}"
--- app/models/vote.rb
+++ app/models/vote.rb
@@ -1,3 +1,3 @@
 class Vote < ApplicationRecord
-	belongs_to :story
+  belongs_to :story
 end
