89fe78404189f6920db19430a8ae12917b373fa7
Jonatan Bonilla <pescadin138@gmail.com>
2018-09-24 22:40:50 -0500

    add authentication to our tests and tests log in and log out functionality

--- test/controllers/stories_controller_test.rb
+++ test/controllers/stories_controller_test.rb
@@ -7,6 +7,7 @@ class StoriesControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should get new" do
+    login_user
     get new_story_path
     assert_response :success
     assert_template 'new'
@@ -14,11 +15,13 @@ class StoriesControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "new shows new form" do
+    login_user
     get new_story_path
     assert_select 'form div', count: 2
   end
 
   test "adds a story" do
+    login_user
     assert_difference "Story.count" do
       post stories_path, params: {
         story: {
@@ -50,5 +53,44 @@ class StoriesControllerTest < ActionDispatch::IntegrationTest
     assert_select 'h2 span#vote_score'
     assert_select 'ul#vote_history li', count: 2
     assert_select 'div#vote_form form'
+  end
+
+  test "show story submitter" do
+    get story_path(stories(:one))
+    assert_select 'p.submitted_by span', 'Glenn Goodrich'
+  end
+
+  test "indicates not logged in" do
+    get stories_path
+    assert_select 'div#login_logout em', 'Not logged in.'
+  end
+
+  test "show navigation menu" do
+    get stories_path
+    assert_select 'ul#navigation li', 2
+  end
+
+  test "indicates logged in user" do
+    login_user
+    get stories_path
+    assert_select 'div#login_logout em a', '(Logout)'
+  end
+
+  test "redirects if not logged in" do
+    get new_story_path
+    assert_response :redirect
+    assert_redirected_to new_session_path
+  end
+
+  test "stores user with story" do
+    login_user
+    post stories_path, params: {
+      story: {
+        name: 'story with user',
+        link: 'http://www.story-with-user.com'
+      }
+    }
+
+    assert_equal users(:glenn), Story.last.user
   end
 end
--- test/models/vote_test.rb
+++ test/models/vote_test.rb
@@ -4,4 +4,8 @@ class VoteTest < ActiveSupport::TestCase
   test 'votes has a story' do
     assert_equal stories(:one), votes(:one).story
   end
+
+  test "is associated with a user" do
+    assert_equal users(:john), votes(:two).user
+  end
 end
