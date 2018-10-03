8e6b6ca2a954fecee7644a147b317483c01f2d2f
Jonatan Bonilla <pescadin138@gmail.com>
2018-10-02 21:48:11 -0500

    add bin route to stories resource

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   root 'stories#index'
   resource :session
   resources :stories do
+    collection do
+      get 'bin'
+    end
     resources :votes do
     end
   end
